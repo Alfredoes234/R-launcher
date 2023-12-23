b8e3010972132ee3fa195c015a2c4a48f499513d
Alfredoes234 <alfie.rayner30@gmail.com>
2023-12-23 20:34:57 +0000

    Initialized

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,23 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Exit");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_submenu(submenu);
+    tauri::Builder::default()
+        .menu(menu)
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { Fragment, render } from "preact";
+import App from "./App";
+import "./styles.css";
+
+
+render(<Fragment><App /></Fragment>, document.getElementById("root")!);
