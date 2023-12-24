6bfe64eef379b9b1426580636cfa6291107eeb8c
Alfredoes234 <alfie.rayner30@gmail.com>
2023-12-24 00:41:45 +0000

    updated it

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-//import { Router } from 'preact-iso/router';
+import { Router } from 'preact-iso/router';
 import { render } from "preact";
 import "./styles.css";
-//import About from './about';
+import About from './about';
 import App from './app';
 
 
 
-//const Routes = () => (
-//    <Router>
-//        <App path="/" />
-//        <About path="/about" />
-//    </Router>
-//);
+const Routes = () => (
+    <Router>
+        <App path="/" />
+        <About path="/about" />
+    </Router>
+);
 
-render(<App />, document.getElementById("root")!);
+render(<Routes />, document.getElementById("root")!);
